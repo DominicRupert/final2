bf3c7be1a0c9c6d01e0fb3da0094bb7f1f8c68d5
dom <dominicrupert@gmail.com>
2022-08-15 17:51:09 -0600

    stuff

--- final2.client/src/components/Navbar.vue
+++ final2.client/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" class="rounded-pill" src="../assets/img/DALL·E 2022-08-14 23.30.13 - a photo-realistic grizzly bear wearing a hoodie programming on visual studio code digital art.png" height="45" />
       </div>
     </router-link>
     <button
@@ -19,12 +19,7 @@
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <!-- <router-link
-            :to="{ name: 'About' }"
-            class="btn text-success lighten-30 selectable text-uppercase"
-          >
-            About
-          </router-link> -->
+    
         </li>
       </ul>
       <!-- LOGIN COMPONENT HERE -->
